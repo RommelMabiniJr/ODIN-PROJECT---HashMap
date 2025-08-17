480192ead6c2c8123418c9749d460de61afaf6df
Rommel Mabini Jr <rommelbrillantesmabinijr@gmail.com>
2025-08-18 07:31:24 +0800

    Add test cases for HashMap: set, get, remove, growth, and reassignment

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
 import HashMap from "./HashMap.js";
-import LinkedList from "./LinkedList.js";
 
-const hashInstance = new HashMap(.75, 16);
+const test = new HashMap(.75, 16);
 
-console.log(hashInstance.hash("Rama"));
-console.log(hashInstance.hash("Sita"));
-// console.log(hashInstance.entries()[0]);
-hashInstance.set("Carlos", "This is an old value");
-console.log(hashInstance.get("Carlos"));
-hashInstance.set("Carlos", "This is the new value");
-console.log(hashInstance.get("Carlos"));
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+console.log(test.length());
+console.log(test.length());
+console.log(test.entries());
+
+// test reassignment
+test.set('grape', 'green')
+console.log(test.get("grape"));
+console.log(test.length());
+console.log(test.entries());
+
+// test growth
+test.set('moon', 'silver')
+console.log(test.length());
+console.log(test.entries());
+
+// test reassignment after growth
+test.set('moon', 'copper');
+console.log(test.length());
+console.log(test.entries());
+
+// test adding a new key after growth
+test.set('night', 'dark blue')
+console.log(test.length());
+console.log(test.entries());
+
+//test removing a key
+console.log(test.remove('apple'));
+console.log(test.length());
+console.log(test.entries());
